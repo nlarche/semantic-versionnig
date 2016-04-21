efb10a8cbc815c603f6cf1bbfc25cfa26aee4b8e
nilar <nicolas.larche@rca.fr>
2016-04-21 15:15:31 +0200

    update code documentation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+'use strict'
 
 var fs = require('fs')
 var log = require('npmlog');
@@ -11,8 +12,9 @@ var userArgs = process.argv.slice(2);
 var semverType = userArgs[0] === undefined ? 'patch' : userArgs[0];
 
 var actions = [
-    // get new version
+    // set new version
     // git commit version
+    // git create tag
     'npm version ' + semverType,
     // git push
    'git push origin master --tags',
@@ -20,43 +22,46 @@ var actions = [
    'npm publish'
 ];
 
+// Cancel Actions in case of Error
 var cancel =[    
+    // delete last tag
     'git tag -d $(git describe --abbrev=0)',
+    // delete last commit
     'git reset --soft HEAD~1',
+    // Reset package.json
     'git reset HEAD package.json',
     'git checkout package.json',
 ] 
 
 var exec = Rx.Observable.fromNodeCallback(child_process.exec);
 
-// var subscription = source.subscribe(
-//   x => console.log('onNext: %s', x),
-//   e => console.log('onError: %s', e),
-//   () => console.log('onCompleted'));
-
 log.heading = 'semantic-versionning'
 log.info('Semantic versionning');
 
 process_action(actions, logError);
 
-// function
+// onError
 function logError(err) {
     log.error(err);
+    // execute delete actions
     process_action(cancel, function err(){
         log.error(err)
     });
 }
 
+// onNext
 function onNext(action) {  
      if (action){
         log.info(action);         
      }
 }
 
+// onComplete
 function completed() {
     log.info('info Semantic versionning completed')
 }
 
+// run actions
 function process_action(actions, errorFn){
     return Rx.Observable.for(actions, function(action) {
         log.info(action)
@@ -64,10 +69,3 @@ function process_action(actions, errorFn){
     })
     .subscribe(onNext, errorFn, completed);
 }
-
-
-
-
-
-
-
